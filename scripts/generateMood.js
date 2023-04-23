94335326f09b3682bf3f6fdd46c8cacaed6f3b3e
SaltyGoo <74217162+SaltyGoo@users.noreply.github.com>
2023-04-23 17:01:55 -0400

    Update generateMood.js

--- scripts/generateMood.js
+++ scripts/generateMood.js
@@ -1,14 +1,16 @@
-function generateMood {
-  // Create an array of six options
-  var options = ["Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6"];
+function generateMood() {
+  // Create an array of six mood options
+  var moods = ["happiness", "anger", "sadness", "excitement", "fear", "surprise"];
 
   // Generate a random index number between 0 and 5
   var index = Math.floor(Math.random() * 6);
 
-  // Get the option at the generated index
-  var selectedOption = options[index];
+  // Get the mood option at the generated index
+  var selectedMood = moods[index];
 
   // Get the paragraph element where we will display the generated text
   var generatedText = document.getElementById("MoodResult");
 
+  // Set the text content of the paragraph element to the selected mood in italic
+  generatedText.innerHTML = "<em>" + selectedMood + "</em>";
 }

ca9b456f9a6114fef3ea5195cb555c059e99b064
SaltyGoo <74217162+SaltyGoo@users.noreply.github.com>
2023-04-24 16:44:46 -0400

    Update hexmap.js

--- CSV/hexmap.js
+++ CSV/hexmap.js
@@ -1,31 +1,28 @@
- const cvsBiomes = ['/CSV/Monster - 01_Arctic.csv', '/CSV/Monster - 02_Desert.csv', '/CSV/Monster - 03_Forest.csv', '/CSV/Monster - 04_Hills.csv', '/CSV/Monster - 05_Jungle.csv', '/CSV/Monster - 06_Mountain.csv', '/CSV/Monster - 07_Plains.csv', '/CSV/Monster - 08_Swamp.csv', '/CSV/Monster - 09_City.csv', '/CSV/Monster - 10_Sea'];
+const cvsBiomes = ['/CSV/Monster - 01_Arctic.csv', '/CSV/Monster - 02_Desert.csv', '/CSV/Monster - 03_Forest.csv', '/CSV/Monster - 04_Hills.csv', '/CSV/Monster - 05_Jungle.csv', '/CSV/Monster - 06_Mountain.csv', '/CSV/Monster - 07_Plains.csv', '/CSV/Monster - 08_Swamp.csv', '/CSV/Monster - 09_City.csv', '/CSV/Monster - 10_Sea'];
 
 // Name of the specific CVS to use for 10% of the time
 const underdarkCvs = '/CSV/Monster - 11_Gate.csv';
 
 async function getRandomCell(csvFile, columnIndex) {
-  const response = await fetch(csvFile);
-  const data = await response.text();
-  const rows = data.split('\n').filter(row => row.trim() !== '');
-  const cells = rows.map(row => {
-    const cell = row.split(',').map(cell => cell.trim())[columnIndex];
-    if (cell.startsWith('<a href=')) {
-      const match = /href="([^"]+)"/.exec(cell);
-      return match ? match[1] : '';
-    } else {
-      return cell;
-    }
-  }).filter(cell => cell !== '');
-  return cells[Math.floor(Math.random() * cells.length)] || '';
+  try {
+    const response = await fetch(csvFile);
+    const data = await response.text();
+    const { data: rows } = Papa.parse(data);
+    const cells = rows.map(row => row[columnIndex].trim()).filter(cell => cell !== '');
+    return cells[Math.floor(Math.random() * cells.length)] || '';
+  } catch (error) {
+    console.error(error);
+    return '';
+  }
 }
 
 async function generateText() {
   const csvFile = cvsBiomes[Math.floor(Math.random() * cvsBiomes.length)];
-  const cells = await Promise.all(Array.from({length: 12}, (_, i) => getRandomCell(csvFile, i + 3)));
+  const cells = await Promise.all(Array.from({ length: 12 }, (_, i) => getRandomCell(csvFile, i + 3)));
 
   // Add content of columns 4-7 of specific CVS 10% of the time
   if (csvFile !== underdarkCvs && Math.random() < 0.1) {
-    const specificCells = await Promise.all(Array.from({length: 4}, (_, i) => getRandomCell(underdarkCvs, i + 3)));
+    const specificCells = await Promise.all(Array.from({ length: 4 }, (_, i) => getRandomCell(underdarkCvs, i + 3)));
     cells.push(...specificCells);
   }
 

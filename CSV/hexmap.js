175d5c41dc205c76025e28488b6423bcf4e16989
SaltyGoo <74217162+SaltyGoo@users.noreply.github.com>
2023-04-24 00:20:56 -0400

    Update hexmap.js

--- CSV/hexmap.js
+++ CSV/hexmap.js
@@ -1,7 +1,7 @@
-const cvsFiles = ['Monster - 01_Arctic.csv', 'Monster - 02_Desert.csv', 'Monster - 03_Forest.csv', 'Monster - 04_Hills.csv', 'Monster - 05_Jungle.csv', 'Monster - 06_Mountain.csv', 'Monster - 07_Plains.csv', 'Monster - 08_Swamp.csv', 'Monster - 09_City.csv', 'Monster - 10_Sea'];
+ const cvsBiomes = ['/CSV/Monster - 01_Arctic.csv', '/CSV/Monster - 02_Desert.csv', '/CSV/Monster - 03_Forest.csv', '/CSV/Monster - 04_Hills.csv', '/CSV/Monster - 05_Jungle.csv', '/CSV/Monster - 06_Mountain.csv', '/CSV/Monster - 07_Plains.csv', '/CSV/Monster - 08_Swamp.csv', '/CSV/Monster - 09_City.csv', '/CSV/Monster - 10_Sea'];
 
 // Name of the specific CVS to use for 10% of the time
-const specificCvs = 'Monster - 11_Gate.csv';
+const underdarkCvs = '/CSV/Monster - 11_Gate.csv';
 
 async function getRandomCell(csvFile, columnIndex) {
   const response = await fetch(csvFile);
@@ -12,12 +12,12 @@ async function getRandomCell(csvFile, columnIndex) {
 }
 
 async function generateText() {
-  const csvFile = cvsFiles[Math.floor(Math.random() * cvsFiles.length)];
+  const csvFile = cvsBiomes[Math.floor(Math.random() * cvsBiomes.length)];
   const cells = await Promise.all(Array.from({length: 12}, (_, i) => getRandomCell(csvFile, i + 3)));
 
   // Add content of columns 4-7 of specific CVS 10% of the time
-  if (csvFile !== specificCvs && Math.random() < 0.1) {
-    const specificCells = await Promise.all(Array.from({length: 4}, (_, i) => getRandomCell(specificCvs, i + 3)));
+  if (csvFile !== underdarkCvs && Math.random() < 0.1) {
+    const specificCells = await Promise.all(Array.from({length: 4}, (_, i) => getRandomCell(underdarkCvs, i + 3)));
     cells.push(...specificCells);
   }
 

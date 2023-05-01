b77e456aed584eb8b19386064c89dac19d260d3d
SaltyGoo <74217162+SaltyGoo@users.noreply.github.com>
2023-05-01 00:42:29 -0400

    Update randomwords.js

--- scripts/randomwords.js
+++ scripts/randomwords.js
@@ -1,6 +1,12 @@
-const words = ['PURGATORY', 'GEHENNA', 'HELL', 'THE ABYSS', 'THE DREAMWORLD', 'HEAVEN', 'THE ELEMENTAL PLANE OF FIRE', 'THE ELEMENTAL PLANE OF AIR', 'THE ELEMENTAL PLANE OF EARTH', 'THE ELEMENTAL PLANE OF WATER', 'THE ASTRAL SEA'];
-const searchWord = 'rdmPLANE';
+const replacements = {
+  'rdmPLANE': ['PURGATORY', 'GEHENNA', 'HELL', 'THE ABYSS', 'THE DREAMWORLD', 'HEAVEN', 'THE ELEMENTAL PLANE OF FIRE', 'THE ELEMENTAL PLANE OF AIR', 'THE ELEMENTAL PLANE OF EARTH', 'THE ELEMENTAL PLANE OF WATER', 'THE ASTRAL SEA'],
+  'word_to_replace_2': ['random4', 'random5', 'random6'],
+  'word_to_replace_3': ['random7', 'random8', 'random9']
+};
 
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp(searchWord, 'g'), () => {
-  return words[Math.floor(Math.random() * words.length)];
+Object.keys(replacements).forEach(searchWord => {
+  document.body.innerHTML = document.body.innerHTML.replace(new RegExp(searchWord, 'g'), () => {
+    const words = replacements[searchWord];
+    return words[Math.floor(Math.random() * words.length)];
+  });
 });

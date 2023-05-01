1c6cadcadd53a3c75c946653ed5b891de7f88923
SaltyGoo <74217162+SaltyGoo@users.noreply.github.com>
2023-05-01 01:04:42 -0400

    Update randomwords.js

--- scripts/randomwords.js
+++ scripts/randomwords.js
@@ -1,7 +1,6 @@
 const replacements = {
   'rdmPLANE': ['PURGATORY', 'GEHENNA', 'HELL', 'THE ABYSS', 'THE DREAMWORLD', 'HEAVEN', 'THE ELEMENTAL PLANE OF FIRE', 'THE ELEMENTAL PLANE OF AIR', 'THE ELEMENTAL PLANE OF EARTH', 'THE ELEMENTAL PLANE OF WATER', 'THE ASTRAL SEA'],
   'word_to_replace_2': ['random4', 'random5', 'random6'],
-  'word_to_replace_3': ['random7', 'random8', 'random9']
 };
 
 Object.keys(replacements).forEach(searchWord => {

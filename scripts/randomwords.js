a84ca0a0b27cfac2085b6e067a47edc926882884
SaltyGoo <74217162+SaltyGoo@users.noreply.github.com>
2023-05-03 21:52:00 -0400

    Update randomwords.js

--- scripts/randomwords.js
+++ scripts/randomwords.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const replacements = {
-    'rdmPLANE': ['PURGATORY', 'GEHENNA', 'HELL', 'THE ABYSS', 'THE DREAMWORLD', 'HEAVEN', 'THE ELEMENTAL PLANE OF FIRE', 'THE ELEMENTAL PLANE OF AIR', 'THE ELEMENTAL PLANE OF EARTH', 'THE ELEMENTAL PLANE OF WATER', 'THE ASTRAL SEA'],
-    'word_to_replace_2': ['random4', 'random5', 'random6'],
-  };
+const replacements = {
+  'rdmPLANE': ['PURGATORY', 'GEHENNA', 'HELL', 'THE ABYSS', 'THE DREAMWORLD', 'HEAVEN', 'THE ELEMENTAL PLANE OF FIRE', 'THE ELEMENTAL PLANE OF AIR', 'THE ELEMENTAL PLANE OF EARTH', 'THE ELEMENTAL PLANE OF WATER', 'THE ASTRAL SEA'],
+  'word_to_replace_2': ['random4', 'random5', 'random6'],
+};
 
-  Object.keys(replacements).forEach(searchWord => {
-    document.body.innerHTML = document.body.innerHTML.replace(new RegExp(searchWord, 'g'), () => {
-      const words = replacements[searchWord];
-      return words[Math.floor(Math.random() * words.length)];
-    });
+const generatedText = document.getElementById('output');
+
+Object.keys(replacements).forEach(searchWord => {
+  generatedText.innerHTML = generatedText.innerHTML.replace(new RegExp(searchWord, 'g'), () => {
+    const words = replacements[searchWord];
+    return words[Math.floor(Math.random() * words.length)];
   });
+});
 });

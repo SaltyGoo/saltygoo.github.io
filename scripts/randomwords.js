94df89a644d69b615841ddc66afb7678e89f6c28
SaltyGoo <74217162+SaltyGoo@users.noreply.github.com>
2023-05-03 22:19:38 -0400

    Update randomwords.js

--- scripts/randomwords.js
+++ scripts/randomwords.js
@@ -1,20 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
   const replacements = {
     'rdmPLANE': ['PURGATORY', 'GEHENNA', 'HELL', 'THE ABYSS', 'THE DREAMWORLD', 'HEAVEN', 'THE ELEMENTAL PLANE OF FIRE', 'THE ELEMENTAL PLANE OF AIR', 'THE ELEMENTAL PLANE OF EARTH', 'THE ELEMENTAL PLANE OF WATER', 'THE ASTRAL SEA'],
     'word_to_replace_2': ['random4', 'random5', 'random6'],
   };
-
-  const button = document.querySelector('button');
-  button.addEventListener('click', async () => {
-    const text = await generateText();
-    const outputDiv = document.querySelector('#output');
-    outputDiv.innerHTML = '<br>' + text;
-    
-    Object.keys(replacements).forEach(searchWord => {
-      outputDiv.innerHTML = outputDiv.innerHTML.replace(new RegExp(searchWord, 'g'), () => {
-        const words = replacements[searchWord];
-        return words[Math.floor(Math.random() * words.length)];
-      });
-    });
-  });
-});

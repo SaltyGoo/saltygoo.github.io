5185168faf18b82aff12dbd3de8f76232fb8ba67
SaltyGoo <74217162+SaltyGoo@users.noreply.github.com>
2023-05-06 17:37:33 -0400

    Update randomwords.js

--- scripts/randomwords.js
+++ scripts/randomwords.js
@@ -1,4 +1,4 @@
   const replacements = {
     'rdmPLANE': ['PURGATORY', 'GEHENNA', 'HELL', 'THE ABYSS', 'THE DREAMWORLD', 'HEAVEN', 'THE ELEMENTAL PLANE OF FIRE', 'THE ELEMENTAL PLANE OF AIR', 'THE ELEMENTAL PLANE OF EARTH', 'THE ELEMENTAL PLANE OF WATER', 'THE ASTRAL SEA'],
-    'word_to_replace_2': ['random4', 'random5', 'random6'],
+    'rdmSIZE': ['', 'GIANT'],
   };

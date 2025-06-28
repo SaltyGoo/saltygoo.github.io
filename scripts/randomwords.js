a80355feb0c7bd15887e53919f05904d630a9b14
SaltyGoo <74217162+SaltyGoo@users.noreply.github.com>
2025-06-28 13:10:43 -0400

    Update randomwords.js

--- scripts/randomwords.js
+++ scripts/randomwords.js
@@ -1,13 +1,34 @@
-  const replacements = {
-    'rdmCULT': ['a FEY-WORSHIPPING FOLK', 'an ELEMENTAL rdmELEMENT', 'a DEMONIC', 'an ANCIENT SECRET', 'AN ELDRITCH', 'A DEATH', 'A DRUG',],
-    'rdmELEMENT': ['EARTH', 'OOZE', 'ALLOY', 'LAVA', 'FIRE', 'SUN', 'GAS', 'AIR', 'STORM', 'FROST', 'WATER', 'FOG'],
-    'rdmPLANE': ['PURGATORY', 'GEHENNA', 'HELL', 'the ABYSS', 'the DREAMWORLD', 'HEAVEN', 'rdmFIRE', 'rdmAIR', 'rdmEARTH', 'rdmWATER', 'the ASTRAL SEA'],
-    'rdmEARTH': ['the PLANE OF EARTH', 'the PLANE OF OOZE', 'the PLANE OF METAL', 'the PLANE OF MAGMA', ],
-    'rdmFIRE': ['the PLANE OF FIRE', 'the PLANE OF LIGHT', 'the PLANE OF SMOKE', 'the PLANE OF MAGMA', ],
-    'rdmAIR': ['the PLANE OF AIR', 'the PLANE OF ELECTRICITY', 'the PLANE OF SMOKE', 'the PLANE OF COLD', ],
-    'rdmWATER': ['the PLANE OF WATER', 'the PLANE OF STEAM', 'the PLANE OF OOZE', 'the PLANE OF COLD', ],
-    'rdmSIZE': ['', 'GIANT'],
-    'rdmNOBLE': ['BARON', 'BARONNESS', 'PRINCE', 'PRINCESS', 'KING', 'QUEEN', 'DUKE', 'DUCHESS', 'MARQUIS', 'MARCHIONESS', 'ARCHBISHOP', 'COUNT', 'COUNTESS', 'BARONNESS', 'EMPEROR', 'EMPRESS', 'PRINCE-ELECTOR'],
-  };
+<script>
+    const replacements = {
+        'rdmCULT': ['a FEY-WORSHIPPING FOLK', 'an ELEMENTAL rdmELEMENT', 'a DEMONIC', 'an ANCIENT SECRET', 'AN ELDRITCH', 'A DEATH', 'A DRUG'],
+        'rdmELEMENT': ['EARTH', 'OOZE', 'ALLOY', 'LAVA', 'FIRE', 'SUN', 'GAS', 'AIR', 'STORM', 'FROST', 'WATER', 'FOG'],
+        'rdmPLANE': ['PURGATORY', 'GEHENNA', 'HELL', 'the ABYSS', 'the DREAMWORLD', 'HEAVEN', 'rdmFIRE', 'rdmAIR', 'rdmEARTH', 'rdmWATER', 'the ASTRAL SEA'],
+        'rdmEARTH': ['the PLANE OF EARTH', 'the PLANE OF OOZE', 'the PLANE OF METAL', 'the PLANE OF MAGMA'],
+        'rdmFIRE': ['the PLANE OF FIRE', 'the PLANE OF LIGHT', 'the PLANE OF SMOKE', 'the PLANE OF MAGMA'],
+        'rdmAIR': ['the PLANE OF AIR', 'the PLANE OF ELECTRICITY', 'the PLANE OF SMOKE', 'the PLANE OF COLD'],
+        'rdmWATER': ['the PLANE OF WATER', 'the PLANE OF STEAM', 'the PLANE OF OOZE', 'the PLANE OF COLD'],
+        'rdmSIZE': ['', 'GIANT'],
+        'rdmNOBLE': ['BARON', 'BARONNESS', 'PRINCE', 'PRINCESS', 'KING', 'QUEEN', 'DUKE', 'DUCHESS', 'MARQUIS', 'MARCHIONESS', 'ARCHBISHOP', 'COUNT', 'COUNTESS', 'BARONNESS', 'EMPEROR', 'EMPRESS', 'PRINCE-ELECTOR'],
+    };
 
-console.log('randomwords.js loaded');
+    function applyReplacements(text) {
+        let replacedText = text;
+        let iterations = 0;
+        const maxIterations = 10;
+        while (iterations < maxIterations) {
+            let changed = false;
+            for (const [key, values] of Object.entries(replacements)) {
+                const regex = new RegExp(key, 'g');
+                if (regex.test(replacedText)) {
+                    replacedText = replacedText.replace(regex, () => {
+                        return values[Math.floor(Math.random() * values.length)];
+                    });
+                    changed = true;
+                }
+            }
+            if (!changed) break;
+            iterations++;
+        }
+        return replacedText;
+    }
+</script>
